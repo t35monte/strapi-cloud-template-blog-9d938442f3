237b0ea950bbc6ab83f16f6a36e54a4ee067eedc
t35monte <aluno28491@ipt.pt>
2026-05-29 22:51:44 +0100

    fix: configurar CORS para localhost:3000

--- config/middlewares.js
+++ config/middlewares.js
@@ -1,9 +1,15 @@
 module.exports = [
-  'strapi::logger',
   'strapi::errors',
-  'strapi::security',
-  'strapi::cors',
+  {
+    name: 'strapi::cors',
+    config: {
+      enabled: true,
+      headers: '*',
+      origin: ['http://localhost:3000'],
+    },
+  },
   'strapi::poweredBy',
+  'strapi::logger',
   'strapi::query',
   'strapi::body',
   'strapi::session',
